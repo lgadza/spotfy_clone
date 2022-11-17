47f5c6bdab7ee1927a5ffd21c5877f38d0569778
Louis Gadza <siuolgadza@gmail.com>
2022-11-17 16:17:08 +0100

    scevs

--- album.js
+++ album.js
@@ -59,74 +59,191 @@ function renderAlbum(album) {
           </div>
     `;
   let playingCover = document.querySelector(".playing-cover");
-  playingCover.src = `${album.cover_small}`;
+  // playingCover.src = `${album.cover_small}`;
   console.log(playingCover);
 }
 const track = document.querySelector(".track");
 function renderAlbumSongs(album) {
+  let likeSongOption = [];
   // let likeOption = [];
   // let songNumberOption = [];
-  // let likeSongOption = [];
-  console.log(album.tracks.data);
-  console.log(album.tracks.data.length);
 
-  let count = 0;
+  //   for (let i = 0; i < album.tracks.data.length; i++) {
+  //     const durationNatural = Number(album.tracks.data[i].duration);
+
+  //     const mins = parseInt(durationNatural / 60);
+  //     const seconds = parseInt(durationNatural - mins * 60);
+  //     track.innerHTML += `
+  //     <div class="d-flex song-bar-options align-items-center py-2 mr-4">
+  //             <span class="song-number">${i + 1}</span>
+  //             <div class=" like-song d-none triangle"></div>
+  //             <div class="d-flex justify-content-between w-100 pr-5">
+  //               <div class="d-flex flex-column ml-5">
+  //                 <span class="track-name">${album.tracks.data[i].title}</span>
+  //                 <span>${album.tracks.data[1].artist.name}</span>
+  //               </div>
+  //               <div class="d-flex align-items-center ml-0 ">
+  //               <span "  id="like-song" class="like-song soong d-none mr-3"><i class="bi bi-heart "></i></span>
+  //                 <span class="song-mins">${mins}:${seconds}</span>
+  //               </div>
+  //               <span class="three-dots d-none"><i class="bi bi-three-dots-vertical"></i></span>
+  //             </div>
+  //           </div>`;
+
+  //     let songBar = document.querySelector(".song-bar-options");
+  //     let songNumber = document.querySelector(".song-number");
+  //     let likeSong = document.querySelectorAll(".soong");
+  //     // likeSong.addEventListener("click", () => {
+  //     //   likeSong.style.color = "red";
+  //     // });
+  //     // $(document).ready(function myFunction() {
+  //     //   likeSong.style.color = "red";
+  //     likeSongOption.push(likeSong);
+  //     // });
+
+  // }
+
   for (let i = 0; i < album.tracks.data.length; i++) {
     const durationNatural = Number(album.tracks.data[i].duration);
-    console.log(durationNatural);
 
     const mins = parseInt(durationNatural / 60);
     const seconds = parseInt(durationNatural - mins * 60);
     track.innerHTML += `
     <div class="d-flex song-bar-options align-items-center py-2 mr-4">
-            <span class="song-number">${i + 1}</span>
-            <span class=" like-song d-none triangle"></span>
-            <div class="d-flex justify-content-between w-100 pr-5">
-              <div class="d-flex flex-column ml-5">
-                <span class="track-name">${album.tracks.data[i].title}</span>
-                <span>${album.tracks.data[1].artist.name}</span>
-              </div>
-              <div class="d-flex align-items-center ml-0 ">
-              <span class="like-song d-none mr-3"><i class="bi bi-heart "></i></span>
-                <span class="song-mins">${mins}:${seconds}</span>
-              </div>
-              <span class="three-dots d-none"><i class="bi bi-three-dots-vertical"></i></span>
-            </div>
-          </div>`;
-    count++;
-    let songBar = document.querySelector(".song-bar-options");
-    let songNumber = document.querySelector(".song-number");
-    let likeSong = document.querySelector(".like-song");
-
-    // console.log(songBar);
-    // likeOption.push(songBar);
-    // likeSongOption.push(likeSong);
-    // songNumberOption.push(songNumber);
+        <span class="song-number">${i + 1}</span>
+        <div onclick="playSong(event)" class=" like-song d-none triangle triangle-play"></div>
+        <div class="d-flex justify-content-between w-100 pr-5">
+          <div class="d-flex flex-column ml-5">
+            <span class="track-name">${album.tracks.data[i].title}</span>
+            <span>${album.tracks.data[1].artist.name}</span>
+          </div>
+          <div class="d-flex align-items-center ml-0 ">
+            <span
+              onclick="makeSongRed(event)"
+              
+              class="like-song soong d-none mr-3"
+            >
+              <i class="bi bi-heart "></i>
+            </span>
+            <span class="song-mins">
+              ${mins}:${seconds}
+            </span>
+          </div>
+          <span class="three-dots d-none">
+            <i class="bi bi-three-dots-vertical"></i>
+          </span>
+        </div>
+      </div> 
+    `;
   }
-
-  let allOptions = [likeOption, likeSongOption, songNumberOption];
-  return allOptions;
-  // likeOption.forEach((songBar) => {
-  //   songBar.addEventListener("mouseover", () => {
-  //     //for (let i = 0; i < album.tracks.data.length; i++) {
-
-  //     songNumberOption.forEach((number) => {
-  //       number.style.display = "none";
-  //     });
-  //     likeSongOption.forEach((song) => {
-  //       song.style.display = "block";
-  //     });
-  //     //}
-  //   });
-  // });
 }
-// console.log(likeOption);
+
+function renderPlayBar(album) {
+  for (let i = 0; i < album.tracks.data.length; i++) {
+    let playingBottomBar = document.querySelector(".playing-bottom-bar");
+    playingBottomBar.innerHTML = `
+  <div class="p-4 d-flex align-items-center">
+        <img class="playing-cover" src="" alt="" />
+        <div class="playing-song-name p-4">
+          <div class="song-name">${album.tracks.data[i].title}</div>
+          <div class="album-name">Queen</div>
+        </div>
+        <div>
+          <iconify-icon
+            icon="mdi:cards-heart-outline"
+            width="12"
+            height="12"
+          ></iconify-icon>
+        </div>
+      </div>
+      <div>
+        <div
+          class="px-4 d-flex align-items-center mr-5 justify-content-center playing-section"
+        >
+          <div class="px-4 ml-5">
+            <iconify-icon icon="fontisto:random" width="12"></iconify-icon>
+          </div>
+          <div class="px-4">
+            <iconify-icon icon="ri:skip-back-fill" width="12"></iconify-icon>
+          </div>
+          <div class="play-triagle">
+            <div class="triangle2"></div>
+          </div>
+          <div class="px-4">
+            <iconify-icon icon="ri:skip-forward-fill" width="12"></iconify-icon>
+          </div>
+          <div class="px-4">
+            <iconify-icon icon="subway:random" width="12"></iconify-icon>
+          </div>
+        </div>
+        <div
+          class="px-4 d-flex align-items-center justify-content-spacebetween"
+        >
+          <span>0:12</span>
+          <div>
+            <div class="progress progress3 mx-4">
+              <div
+                class="progress-bar w-75"
+                role="progressbar"
+                aria-valuenow="75"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <span>3:30</span>
+        </div>
+      </div>
+      <div class="d-flex align-items-center">
+        <div>
+          <iconify-icon icon="fontisto:play-list" width="12"></iconify-icon>
+        </div>
+        <div class="px-4">
+          <iconify-icon icon="mdi:monitor-speaker" width="12"></iconify-icon>
+        </div>
+        <div>
+          <iconify-icon icon="charm:sound-up" width="12"></iconify-icon>
+        </div>
+        <div>
+          <div class="progress progress2 mx-4">
+            <div
+              class="progress-bar w-75"
+              role="progressbar"
+              aria-valuenow="75"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+      </div>
+  `;
+  }
+}
+const makeSongRed = (event) => {
+  const likes = document.querySelectorAll(".soong");
+  for (let i = 0; i < likes.length; i++) {
+    likes[i].addEventListener("click", () => {
+      likes[i].style.color = "red";
+    });
+  }
+};
+const playSong = (event) => {
+  const songs = document.querySelectorAll(".triangle-play");
+  for (let i = 0; i < songs.length; i++) {
+    songs[i].addEventListener("click", () => {
+      songs[i].style.borderColor = "transparent transparent transparent green";
+      songs[i].style.display = "block !important";
+      console.log(songs[i]);
+      // console.log(i + 1);
+      return i;
+    });
+  }
+};
 
 window.onload = async () => {
   const album = await getAlbum();
   renderAlbum(album);
   renderAlbumSongs(album);
+  // const likeSongOption = renderAlbumSongs();
+  renderPlayBar(album);
 };
-// @media screen and (min-width: 480px){
-
-// }
